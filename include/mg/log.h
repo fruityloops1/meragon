9173ffac4b4b65be024f710caea3ce2380322e98
Fruityloops <fruityloops@littun.co>
2022-04-10 20:47:51 +0200

    printf logging function for citra

---
+++ include/mg/log.h
@@ -0,0 +1,19 @@
+#pragma once
+
+extern "C" {
+#include <3ds/svc.h>
+}
+#include <stdio.h>
+
+namespace mg {
+
+template <typename... Args>
+void log(const char* fmt, Args... args)
+{
+    size_t size = snprintf(nullptr, 0, fmt, args...);
+    char data[size + 1];
+    sprintf(data, fmt, args...);
+    svcOutputDebugString(data, size);
+}
+
+} // namespace mg
